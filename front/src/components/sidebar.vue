94005817a5e1fd886d672cbb173c50ecc5af88c4
smoka <smoka7@hotmail.com>
2021-10-10 15:46:42 +0330

    refactor sidebar tabs

--- front/src/components/sidebar.vue
+++ front/src/components/sidebar.vue
@@ -1,10 +1,12 @@
 <template>
-  <div
+  <TabGroup
+    as="div"
     class="
       flex flex-col
       app-height
       overflow-y-auto
       p-2
+      pt-1
       rounded
       bg-white
       m-1
@@ -12,42 +14,48 @@
       dark:bg-gray-700 dark:text-white dark:hover:text-primary
     "
   >
-    <div class="flex w-full my-1 justify-around">
-      <button
-        :class="activeTab == 0 ? tabClasses[0] : tabClasses[1]"
-        @click="changeTab(0)"
-      >
-        <font-awesome-icon icon="list-ul"></font-awesome-icon>
-        <span> playlists </span>
-      </button>
-      <button
-        :class="activeTab == 1 ? tabClasses[0] : tabClasses[1]"
-        @click="changeTab(1)"
-      >
-        <font-awesome-icon icon="folder"></font-awesome-icon>
-        <span> folders </span>
-      </button>
-      <button
-        :class="activeTab == 2 ? tabClasses[0] : tabClasses[1]"
-        @click="changeTab(2)"
-      >
-        <font-awesome-icon icon="search"></font-awesome-icon>
-        <span> search</span>
-      </button>
+    <TabList as="div" class="flex w-full justify-around p-1">
+      <Tab v-slot="{ selected }">
+        <button :class="selected ? tabClasses[0] : tabClasses[1]">
+          <font-awesome-icon icon="list-ul"></font-awesome-icon>
+          <span> playlists </span>
+        </button>
+      </Tab>
+      <Tab v-slot="{ selected }">
+        <button :class="selected ? tabClasses[0] : tabClasses[1]">
+          <font-awesome-icon icon="folder"></font-awesome-icon>
+          <span> folders </span>
+        </button>
+      </Tab>
+      <Tab v-slot="{ selected }">
+        <button :class="selected ? tabClasses[0] : tabClasses[1]">
+          <font-awesome-icon icon="search"></font-awesome-icon>
+          <span> search</span>
+        </button>
+      </Tab>
       <button
         class="p-2 px-6 md:hidden text-primary dark:text-lightest"
         @click="closeFolders"
       >
         <font-awesome-icon icon="times" size="2x"></font-awesome-icon>
       </button>
-    </div>
-    <storedPlaylist v-if="activeTab == 0"></storedPlaylist>
-    <folders v-if="activeTab == 1"></folders>
-    <search v-if="activeTab == 2"></search>
-  </div>
+    </TabList>
+    <TabPanels>
+      <TabPanel>
+        <storedPlaylist></storedPlaylist>
+      </TabPanel>
+      <TabPanel>
+        <folders></folders>
+      </TabPanel>
+      <TabPanel>
+        <search></search>
+      </TabPanel>
+    </TabPanels>
+  </TabGroup>
 </template>
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
 import storedPlaylist from "./storedPlaylist.vue";
 import folders from "./folders.vue";
 import search from "./search.vue";
@@ -58,10 +66,14 @@ export default {
     storedPlaylist,
     folders,
     search,
+    Tab,
+    TabList,
+    TabPanel,
+    TabGroup,
+    TabPanels,
   },
   data() {
     return {
-      activeTab: 0,
       tabClasses: [
         "md:p-2 p-3 bg-primary text-foreground dark:bg-lightest dark:text-primary rounded",
         "md:p-2 p-3 cursor-pointer text-primary dark:text-foreground hover:bg-blue-200 dark:hover:text-primary rounded",
@@ -69,9 +81,6 @@ export default {
     };
   },
   methods: {
-    changeTab(id) {
-      this.activeTab = id;
-    },
     closeFolders: toggleFolders,
   },
 };
