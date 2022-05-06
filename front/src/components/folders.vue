70535568e4d302805a410af3d913d489798bfe02
mohsen <smoka7@hotmail.com>
2022-05-07 00:13:41 +0430

    refactor:update server folders to composition

--- front/src/components/folders.vue
+++ front/src/components/folders.vue
@@ -1,27 +1,16 @@
 <template>
-  <div class="flex cursor-pointer flex-col">
-    <Folder v-for="(folder, index) in folders" :key="index" :data="folder" />
+  <div class="flex cursor-pointer flex-col p-2">
+    <Folder v-for="(folder, index) in serverRoot" :key="index" :data="folder" />
   </div>
 </template>
-<script>
-import endpoints from "../endpoints";
-import { sendCommand } from "../helpers";
-import { shallowReactive } from "vue";
-import Folder from "./folder.vue";
-export default {
-  components: {
-    Folder,
-  },
-  data() {
-    return {
-      folders: shallowReactive([]),
-    };
-  },
-  async created() {
-    const resp = await sendCommand(endpoints.folders, "list", {
-      playlist: "",
-    });
-    this.folders = [...resp.Folders, ...resp.Files];
-  },
-};
+<script setup>
+import endpoints from '../endpoints';
+import { sendCommand } from '../helpers';
+import { shallowReactive } from 'vue';
+import Folder from './folder.vue';
+const resp = await sendCommand(endpoints.folders, 'list', {
+  playlist: '',
+});
+
+const serverRoot = shallowReactive([...resp.Folders, ...resp.Files]);
 </script>
