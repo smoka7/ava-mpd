5af9343b1f9c6ac379a63088fbdad53f04280400
mohsen <smoka7@hotmail.com>
2022-05-08 17:07:17 +0430

    refactor:update queue to composition

--- front/src/components/playlist.vue
+++ front/src/components/playlist.vue
@@ -1,23 +1,21 @@
 <template>
-  <div
-    class="fbg flex h-full w-full flex-col overflow-hidden rounded bg-white/60 backdrop-blur-3xl dark:bg-gray-700/60"
-  >
+  <div class="flex h-full w-full flex-col overflow-y-auto">
     <div
       v-if="queue.Length == 0"
-      class="decoration-accent flex h-full w-full items-center justify-center p-4 text-7xl underline md:text-9xl"
+      class="flex h-full w-full items-center justify-center p-4 text-7xl underline decoration-accent md:text-9xl"
     >
       {{ message }}
     </div>
     <div
-      v-if="!songInfo"
-      class="bg-secondary text-primary h-content absolute bottom-0 z-10 flex w-full flex-wrap-reverse items-center justify-between space-x-4 space-y-2 px-4 text-base md:h-6 md:text-sm"
+      v-if="!state.songInfo"
+      class="h-content absolute bottom-0 z-10 flex w-full flex-wrap-reverse items-center justify-between space-x-4 space-y-2 rounded-b bg-secondary px-4 text-base text-primary md:h-6 md:text-sm"
     >
       <span>
         {{ queue.Length }} Tracks / duration:
         {{ humanizeTime(queue.Duration) }}
       </span>
-      <span v-if="selectedIds.length > 0">
-        {{ selectedIds.length }} selected
+      <span v-if="state.selectedIds.length > 0">
+        {{ state.selectedIds.length }} selected
       </span>
       <queuePagination @goToCurrent="scrollToCurrentSong" />
     </div>
@@ -27,7 +25,7 @@
         :key="index"
         :album="album"
         :currentAlbum="currentAlbum(album)"
-        :selectedIds="selectedIds"
+        :selectedIds="state.selectedIds"
         :currentSongPos="currentSongPos"
         @showMenu="showMenu"
         @select="selectSong"
@@ -35,98 +33,110 @@
     </div>
     <teleport to="#app">
       <queue-menu
-        :open="menu"
-        :song="selected"
-        :selected-ids="selectedIds"
+        :open="state.menu"
+        :song="state.selected"
+        :selected-ids="state.selectedIds"
         @close="hideMenu"
-        @clearSelection="selectedIds = []"
-        @showInfo="songInfo = true"
+        @clearSelection="state.selectedIds = []"
+        @showInfo="state.songInfo = true"
       />
     </teleport>
-    <songInfo v-if="songInfo" :song="selected.id" @close="songInfo = false" />
+    <songInfo
+      v-if="state.songInfo"
+      :song="state.selected.id"
+      @close="state.songInfo = false"
+    />
   </div>
 </template>
-<script>
-import { mapState } from "vuex";
-import { humanizeTime } from "../helpers.js";
-import { defineAsyncComponent, shallowReactive } from "vue";
-import album from "./album.vue";
-import queuePagination from "./queuePagination.vue";
-export default {
-  components: {
-    album,
-    queuePagination,
-    songInfo: defineAsyncComponent(() => import("./songInfo.vue")),
-    queueMenu: defineAsyncComponent(() => import("./queueMenu.vue")),
-  },
-  data() {
-    return {
-      menu: false,
-      songInfo: false,
-      selected: { id: -1, pos: -1 },
-      selectedIds: [],
-    };
-  },
-  async mounted() {
-    await this.$store.dispatch("getQueue");
-    this.scrollToCurrentSong();
-  },
-  methods: {
-    humanizeTime: humanizeTime,
-    showMenu(pos, id) {
-      this.selected.pos = Number(pos);
-      this.selected.id = Number(id);
-      this.menu = true;
-    },
-    hideMenu() {
-      this.menu = false;
-    },
-    selectSong(id) {
-      const index = this.selectedIds.indexOf(id);
-      if (index > -1) {
-        this.selectedIds.splice(index, 1);
-        return;
-      }
-      this.selectedIds.push(id);
-    },
-    async scrollToCurrentSong() {
-      if (this.queue.CurrentPage != this.queue.currentSongPage) {
-        await this.$store.dispatch("getQueue", this.queue.currentSongPage);
-      }
-      const el = document.getElementById("currentSong");
-      if (el) el.scrollIntoView({ block: "center", behavior: "smooth" });
-    },
-    currentAlbum(album) {
-      return (
-        Number(this.currentSongPos) >= Number(album.Songs[0].Pos) &&
-        Number(this.currentSongPos) <=
-          Number(album.Songs[album.Songs.length - 1].Pos)
-      );
-    },
-  },
-  computed: {
-    ...mapState({
-      queue: (state) => shallowReactive(state.queue),
-      currentSongPos: (state) => state.currentSong.Info.Pos,
-      message: (state) => {
-        if (!state.connected) {
-          return "Couldn't Connect to server!!";
-        }
-        if (state.queue.Length == 0) {
-          return "Queue is empty!";
-        }
-      },
-    }),
-  },
-  watch: {
-    currentSongPos: function() {
-      this.scrollToCurrentSong();
-    },
-  },
-};
+<script setup>
+import { useStore } from 'vuex';
+import { humanizeTime } from '../helpers.js';
+import {
+  defineAsyncComponent,
+  shallowReactive,
+  reactive,
+  watch,
+  computed,
+  onMounted,
+} from 'vue';
+import album from './album.vue';
+import queuePagination from './queuePagination.vue';
+
+const songInfo = defineAsyncComponent(() => import('./songInfo.vue'));
+const queueMenu = defineAsyncComponent(() => import('./queueMenu.vue'));
+
+const store = useStore();
+
+await store.dispatch('getQueue');
+
+const state = reactive({
+  menu: false,
+  songInfo: false,
+  selected: { id: -1, pos: -1 },
+  selectedIds: [],
+});
+
+const queue = computed(() => shallowReactive(store.state.queue));
+
+const currentSongPos = computed(() => {
+  return store.state.currentSong.Info.Pos;
+});
+
+const message = computed(() => {
+  if (!store.state.connected) {
+    return "Couldn't Connect to server!!";
+  }
+  if (queue.Length == 0) {
+    return 'Queue is empty!';
+  }
+  return '';
+});
+
+onMounted(() => {
+  scrollToCurrentSong();
+});
+
+function showMenu(pos, id) {
+  state.selected.pos = Number(pos);
+  state.selected.id = Number(id);
+  state.menu = true;
+}
+
+function hideMenu() {
+  state.menu = false;
+}
+
+function selectSong(id) {
+  const index = state.selectedIds.indexOf(id);
+  if (index > -1) {
+    state.selectedIds.splice(index, 1);
+    return;
+  }
+  state.selectedIds.push(id);
+}
+
+async function scrollToCurrentSong() {
+  if (queue.CurrentPage != queue.currentSongPage) {
+    await state.$store.dispatch('getQueue', queue.currentSongPage);
+  }
+  const el = document.getElementById('currentSong');
+  if (el) el.scrollIntoView({ block: 'center', behavior: 'smooth' });
+}
+
+function currentAlbum(album) {
+  return (
+    Number(this.currentSongPos) >= Number(album.Songs[0].Pos) &&
+    Number(this.currentSongPos) <=
+      Number(album.Songs[album.Songs.length - 1].Pos)
+  );
+}
+
+watch(currentSongPos, () => {
+  scrollToCurrentSong();
+});
 </script>
 <style>
 #currentSong {
-  @apply dark:text-primary bg-red-300 !important;
+  @apply bg-red-300 dark:text-primary !important;
 }
 </style>
